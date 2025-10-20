f97c37baa1dc79ec85362a2f00fb2af19bf45a50
dawtom97 <dawtom97@gmail.com>
2025-10-20 19:54:12 +0200

    open weather file created

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-from config import Config
+from services.open_weather import fetch_weather
 
-print(Config.WEATHER_API_KEY)
-print(Config.CITY)
+fetch_weather()
 
-
-# stworzenie repozytorium
-# git init
-
-# sprawdzenie stanu repozytorium
-# git status
-
-# dodanie pliku do repo
-# git add nazwa
-
-# dodanie wszystkiego
-# git add .
-
-# stworzenie commita - punktu w czasie do którego można wracać
-# git commit -m "wiadomosc"
-
-# lista commitów
-# git log --oneline
-
-# podróżowanie po commitach
-# git checkout id_commita
-
-# powrót do teraźniejszości
-# git checkout -
