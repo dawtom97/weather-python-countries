378e89b36266b6050a9b6854a7fa944e57117e4a
dawtom97 <dawtom97@gmail.com>
2025-10-20 18:08:20 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    WEATHER_API_KEY = "912c622485ebcccfe6e75ebb3dc2de10"
+    CITY = "Lisbon"
+    EXCEL_FILEPATH = "pogoda.xlsx"
+
+
+testowa = 10
---
+++ main.py
@@ -0,0 +1,18 @@
+from config import Config
+
+print(Config.WEATHER_API_KEY)
+print(Config.CITY)
+
+
+# stworzenie repozytorium
+# git init
+
+# sprawdzenie stanu repozytorium
+# git status
+
+# dodanie pliku do repo
+# git add nazwa
+
+# dodanie wszystkiego
+# git add .
+
